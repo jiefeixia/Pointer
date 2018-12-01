5a05c9a9da44c252f118b46ebe76d03604bc76c7
gropies <plzmaggie@gmail.com>
2018-12-01 13:15:10 -0500

    upload downloading method(us_rent)

---
+++ download.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from time import sleep
+import os
+
+
+def us_rent():
+    mypath = os.path.join(os.getcwd(),"data")
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory': mypath}
+    options.add_experimental_option('prefs', prefs)
+
+    driver = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
+    driver.get('https://www.zillow.com/home-values/')
+    sleep(2)
+    driver.find_element_by_link_text("View Data Table").click()
+    driver.implicitly_wait(10)
+    driver.find_element_by_xpath('//a[text()="Download Full Data"]').click()
+
+    sleep(5)
+
+
+
