2e03303cd377e8f96a5d77183205f261ea422952
jiefeixia <jiefeix@andrew.cmu.edu>
2018-12-02 02:48:05 -0500

    fix bugs

--- Point_v1.py
+++ Point_v1.py
@@ -5,6 +5,7 @@ from wordcloud import WordCloud, STOPWORDS
 import numpy as np
 import plotly.graph_objs as go
 from plotly.offline import iplot
+import plotly
 
 import glassdoor
 import heinz_course_api
@@ -12,21 +13,26 @@ import smartevals
 import geo
 import rent
 
-from sklearn import feature_extraction  
-from sklearn.feature_extraction.text import TfidfTransformer  
-from sklearn.feature_extraction.text import CountVectorizer 
+from sklearn import feature_extraction
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 
 """
 this is the main function
 """
 
-CONSULTING_FILE = "data/consultant.csv"
-SDE_FILE = "data/sde.csv"
-DS_FILE = "data/ds.csv"
+# CONSULTING_FILE = "data/consultant.csv"
+# SDE_FILE = "data/sde.csv"
+# DS_FILE = "data/ds.csv"
 COURSE_EVL_FILE = "data/course_evl.csv"
 COURSE_INFO_FILE = "data/course_info.csv"
 COMPANY_LOC_FILE = "data/company_loc.csv"
 RENT_FILE = "data/rent_price.csv"
+
+# test file is less than 100 item, used for debug
+CONSULTING_FILE = "data/consultant_test.csv"
+SDE_FILE = "data/sde_test.csv"
+DS_FILE = "data/ds_test.csv"
 
 
 class Person:
@@ -41,55 +47,58 @@ class Person:
         course = []
         for i in range(len(courseout)):
             if (type(coursedes[i]) != str):
-                coursedes[i] = str(coursedes[i]) #make sure data type are string
+                coursedes[i] = str(coursedes[i])  # make sure data type are string
             if (type(courseout[i]) != str):
-                courseout[i] = str(courseout[i]) #make sure data type are string
-            course.append(coursedes[i] + courseout[i]) # combine description and outcome together
-       
+                courseout[i] = str(courseout[i])  # make sure data type are string
+            course.append(coursedes[i] + courseout[i])  # combine description and outcome together
+
         # deal with stopwords
         import codecs
-        stop= open('corenlp_stopwords.txt',encoding = 'utf-8')
+        stop = open('corenlp_stopwords.txt', encoding='utf-8')
         st = stop.readlines()
         for i in range(len(st)):
-            st[i]=st[i].strip('\n') # delete '\n' in each word
-        st.extend(['allow','outcomes','some','mini','another','student','in','or','either','final','exam','mid','by',
-                  'areas','also','today','course','Outcomes:','will','Work','Students','The','It','us','class','Learning',
-                  'course,'])
+            st[i] = st[i].strip('\n')  # delete '\n' in each word
+        st.extend(
+            ['allow', 'outcomes', 'some', 'mini', 'another', 'student', 'in', 'or', 'either', 'final', 'exam', 'mid',
+             'by',
+             'areas', 'also', 'today', 'course', 'Outcomes:', 'will', 'Work', 'Students', 'The', 'It', 'us', 'class',
+             'Learning',
+             'course,'])
         for i in range(len(course)):
             out = ''
             for word in course[i].split(' '):
                 if word not in st:
                     out = out + word + ' '
             course[i] = out
-            
+
         # get tfidf and extract key words
-        vectorizer = CountVectorizer()#
-        transformer = TfidfTransformer()#                                                                                                                                                                                                                                                                               词语的tf-idf权值  
-        tfidf = transformer.fit_transform(vectorizer.fit_transform(course))#calculate tf-idf
-        word = vectorizer.get_feature_names()# all words in the word bag
-        weight=tfidf.toarray()# get matrix of tfidf
+        vectorizer = CountVectorizer()  #
+        transformer = TfidfTransformer()  # 词语的tf-idf权值
+        tfidf = transformer.fit_transform(vectorizer.fit_transform(course))  # calculate tf-idf
+        word = vectorizer.get_feature_names()  # all words in the word bag
+        weight = tfidf.toarray()  # get matrix of tfidf
         key = []
         for i in range(len(weight)):
             doc = {}
             t = np.argsort(-weight[i])
             s = ''
             for j in range(40):
-                #print(word[t[j]], ':', weight[i][t[j]])
+                # print(word[t[j]], ':', weight[i][t[j]])
                 s = s + ' ' + word[t[j]]
             key.append(s)
-            
+
         # print recommended courses based on skill sets matched  
         recommended = []
         for i in range(len(key)):
             for word in skillsinput.split(' '):
                 if (word in key[i]):
-                    recommended.append(course_merged['course_id'][i] + ':' + course_merged['names'][i] + ',course rate:' + '%.2f' % (f_merged['Overall course rate'][i]))
+                    recommended.append(
+                        course_merged['course_id'][i] + ':' + course_merged['names'][i] + ',course rate:' + '%.2f' % (
+                        f_merged['Overall course rate'][i]))
         print("COURSES RECOMMENDED:")
         for i in recommended:
             print(i)
-     
-    
-    
+
     def recommend_course(self, jobs_df, courses_df):
         # TODO(liwei)
         pass
@@ -152,17 +161,16 @@ class Data:
         else:  # career == 'consultant'
             df = self.consulting_df
 
-        salary_ds = df.loc[:, ['salary_low', 'salary_high']]
-        df['avg_salary'] = salary_ds.mean(axis=1)
-        boot_df = df['avg_salary'].sample(frac=10, replace=True)
-        filter_salary_ds = boot_df[np.abs(boot_df['avg_salary'] - boot_df['avg_salary'].mean())
-                                   <= (3 * boot_df['avg_salary'].std())]
+        df = df.loc[:, ["name", 'salary_low', 'salary_high']]
+        df['avg_salary'] = df.mean(axis=1)
+        boot_df = df['avg_salary'].sample(frac=10, replace=True)  # bootstrap to increase sample
+        boot_df = boot_df[np.abs(boot_df - boot_df.mean()) <= (3 * boot_df.std())]  # remove outlier (>3sigma)
         fig, ax = plt.subplots()
-        filter_salary_ds.plot.kde(ax=ax, legend=False, title='Salary for Data Scientist')
-        filter_salary_ds.plot.hist(density=True, ax=ax)
-        ax.set_ylabel('Probability')
+        boot_df.plot.kde(ax=ax, legend=False, title='Salary for Data Scientist')
+        boot_df.plot.hist(density=True, ax=ax)
         ax.grid(axis='y')
         ax.set_facecolor('#d8dcd6')
+        plt.show()
 
     def hist_review(self, career):
         if career == "data scientist":
@@ -175,9 +183,9 @@ class Data:
         fig, ax = plt.subplots()
         df['company_review'].plot.kde(ax=ax, legend=False, title='Company Review Score for Data Scientist')
         df['company_review'].plot.hist(density=True, ax=ax, color='skyblue')
-        ax.set_ylabel('Probability')
         ax.grid(axis='y')
         ax.set_facecolor('#d8dcd6')
+        plt.show()
 
     def job_wc(self, career):
         if career == "data scientist":
@@ -205,7 +213,7 @@ class Data:
         plt.imshow(wc, interpolation="bilinear")
         plt.axis("off")
 
-    def heat_map(self, career):
+    def job_map(self, career):
         if career == 'data scientist':
             df = self.ds_df
         elif career == 'software engineer developer':
@@ -219,20 +227,19 @@ class Data:
         df['cnt'] = pd.to_numeric(df['cnt']).astype(float)
 
         # draw map
-        dat = dict(type='choropleth',
-                   colorscale='Viridis',
-                   locations=df['state'],
-                   z=df['cnt'],
-                   locationmode='USA-states',
-                   marker=dict(line=dict(color='rgb(255,255,255)', width=2)),
-                   colorbar={'title': "Count of jobs"}
-                   )
-        layout = dict(title='Consultant Job Distribution around US',
-                      geo=dict(scope='usa',
-                               showlakes=True))
+        map_data = dict(type='choropleth',
+                    colorscale='Viridis',
+                    locations=df['state'],
+                    z=df['cnt'],
+                    locationmode='USA-states',
+                    marker=dict(line=dict(color='rgb(255,255,255)', width=2)),
+                    colorbar={'title': "Count of jobs"}
+                    )
 
-        choromap = go.Figure(data=[dat], layout=layout)
-        iplot(choromap, validate=False)
+        layout = dict(title='Consultant Job Distribution around US',
+                      geo=dict(scope='usa', showlakes=True))
+        choromap = go.Figure(data=[map_data], layout=layout)
+        plotly.offline.plot(choromap, filename='image/job_map.html')
 
     def rent_map(self):
         map_data = dict(type='choropleth',
@@ -249,7 +256,7 @@ class Data:
                                showlakes=True))
 
         choromap = go.Figure(data=[map_data], layout=layout)
-        iplot(choromap, validate=False)
+        plotly.offline.plot(choromap, filename='image/rent_map.html')
 
 
 if __name__ == "__main__":
@@ -262,15 +269,15 @@ if __name__ == "__main__":
         if not Path(file).is_file():
             Data.update(file)
 
-    data = Data(*files_list)
+    database = Data(*files_list)
 
     # instantiate Person
     person = Person(input("Please enter your career:").lower(), input("Please enter your location:").lower())
 
     # recommend
-    person.recommend_job()
-    person.recommend_course()
-    data.hist_salary(person.career)
-    data.hist_review(person.career)
-    data.job_wc(person.career)
-    data.heat_map(person.career)
+    # person.recommend_job()
+    # person.recommend_course()
+    # database.hist_salary(person.career)
+    # database.hist_review(person.career)
+    # database.job_wc(person.career)
+    # database.heat_map(person.career)
