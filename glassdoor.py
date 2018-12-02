87015ff46edd516e83c5e4779d41fee95aec82f2
jiefeixia <jiefeix@andrew.cmu.edu>
2018-12-02 00:14:18 -0500

    final version

--- glassdoor.py
+++ glassdoor.py
@@ -8,29 +8,35 @@ from multiprocessing import Pool
 import os
 from functools import partial
 
-MAX_PAGE = 15  # max crawling page
+MAX_PAGE = 3  # max crawling page
 
 
 def crawl(file, job, level="entrylevel", max_page=MAX_PAGE):
 
     with Pool(os.cpu_count()) as p:
-        partial_crawl_page = partial(crawl_page,  job, level)
+        partial_crawl_page = partial(crawl_page, job=job, level=level)
         page_list = p.map(partial_crawl_page, [page_num for page_num in range(1, max_page + 1)])
 
     total = pd.DataFrame()
     for page_df in page_list:
         total = total.append(page_df, ignore_index=True)
 
-    print("finish crawling " + str(total.shape[0]) + " items")
+    print("Save " + str(total.shape[0]) + " items about " + job)
     total.to_csv(file, index=False)
 
 
 # single thread function
-def crawl_page(job, level, page_num):
-    start_url = "https://www.glassdoor.com/Job/" + job + "-jobs-SRCH_KO0, 10_IP" + str(page_num) + ".htm?jobType=" + level
-    options = Options()
-    options.headless = True
-    driver = webdriver.Chrome(options=options)
+def crawl_page(page_num, job, level="entrylevel", chrome_headless=False):
+    start_url = "https://www.glassdoor.com/Job/" + job.replace(" ", "-") + "-jobs-SRCH_KO0," + \
+                str(len(job.replace(" ", "-"))) + "_IP" + str(page_num) + ".htm?jobType=" + level
+
+    if chrome_headless:  # show chrome GUI when in debug mode
+        driver = webdriver.Chrome()
+    else:
+        options = Options()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
+
     driver.get(start_url)
 
     names = []
@@ -52,8 +58,7 @@ def crawl_page(job, level, page_num):
             print("successfully close popup")
 
         try:
-            name = driver.find_elements_by_css_selector("#HeroHeaderModule > div.empWrapper > div.header > h1")[
-                0].text
+            name = driver.find_elements_by_css_selector("#HeroHeaderModule > div.empWrapper > div.header > h1")[0].text
             company = driver.find_elements_by_css_selector(
                 "#HeroHeaderModule > div.empWrapper > div.compInfo > a")[0].text
             try:
@@ -86,16 +91,16 @@ def crawl_page(job, level, page_num):
             cnt += 1
             print("finish crawling item ", cnt, " on page ", page_num)
 
-        except StaleElementReferenceException:
-            print("StaleElementReferenceException, Pass")
-            pass
         except IndexError:
             driver.find_elements_by_css_selector("#JAModal > div > div.prettyEmail.modalContents > div.xBtn")[
                 0].click()  # close popup
             print("successfully close popup")
 
+        except StaleElementReferenceException:
+            print("StaleElementReferenceException, Pass")
+            pass
+
     driver.close()
-    print("finish page ", page_num)
 
     page = pd.DataFrame({"name": names, "company": companies, "company_review": company_reviews,
                          "est_salary": est_salaries, "location": locations, "description": descriptions})
@@ -104,9 +109,10 @@ def crawl_page(job, level, page_num):
     page['company_review'] = page['company_review'].replace('★', '', regex=True)
     page['company_review'].fillna("", inplace=True)
     page['est_salary'].fillna("", inplace=True)
-    salary_range = page['est_salary'].str.split("-", n=1, expand=True)
+    salary_range = page['est_salary'].astype(str).str.split("-", n=1, expand=True)
     page['salary_low'] = salary_range[0].map(min_salary)
     page['salary_high'] = salary_range[1].map(max_salary)
+    page = page.drop("est_salary", axis=1)
 
     names.clear()
     companies.clear()
@@ -115,6 +121,7 @@ def crawl_page(job, level, page_num):
     est_salaries.clear()
     descriptions.clear()
 
+    print("finish page ", page_num)
     return page
 
 
@@ -151,4 +158,5 @@ def max_salary(amount):
 
 # for test purpose
 if __name__ == "__main__":
-    crawl("data/test.csv", "consultant", max_page=3)
+    # crawl("data/test.csv", "software engineer", max_page=1)
+    crawl_page(1, "software engineer", chrome_headless=True)
