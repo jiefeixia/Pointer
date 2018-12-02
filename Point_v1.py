d9948d4911e1a016824a6c4e4c6d8a967d2b2bb8
Weiii-Li <45047515+Weiii-Li@users.noreply.github.com>
2018-12-02 01:10:50 -0500

    add  course recommend func on skills input
    user can also choose input skills they want to learn to get a more specific recommend

--- Point_v1.py
+++ Point_v1.py
@@ -30,6 +30,62 @@ class Person:
         self.career = career
         self.location = location
 
+    def recommend_course_onskills(skillsinput, course_merged):
+        # skillsinput is the skills user want to learn, input seperated by space
+        coursedes = list(course_merged['description'])
+        courseout = list(course_merged['learning_outcome'])
+        course = []
+        for i in range(len(courseout)):
+            if (type(coursedes[i]) != str):
+                coursedes[i] = str(coursedes[i]) #make sure data type are string
+            if (type(courseout[i]) != str):
+                courseout[i] = str(courseout[i]) #make sure data type are string
+            course.append(coursedes[i] + courseout[i]) # combine description and outcome together
+       
+        # deal with stopwords
+        import codecs
+        stop= open('corenlp_stopwords.txt',encoding = 'utf-8')
+        st = stop.readlines()
+        for i in range(len(st)):
+            st[i]=st[i].strip('\n') # delete '\n' in each word
+        st.extend(['allow','outcomes','some','mini','another','student','in','or','either','final','exam','mid','by',
+                  'areas','also','today','course','Outcomes:','will','Work','Students','The','It','us','class','Learning',
+                  'course,'])
+        for i in range(len(course)):
+            out = ''
+            for word in course[i].split(' '):
+                if word not in st:
+                    out = out + word + ' '
+            course[i] = out
+            
+        # get tfidf and extract key words
+        vectorizer = CountVectorizer()#
+        transformer = TfidfTransformer()#                                                                                                                                                                                                                                                                               词语的tf-idf权值  
+        tfidf = transformer.fit_transform(vectorizer.fit_transform(course))#calculate tf-idf
+        word = vectorizer.get_feature_names()# all words in the word bag
+        weight=tfidf.toarray()# get matrix of tfidf
+        key = []
+        for i in range(len(weight)):
+            doc = {}
+            t = np.argsort(-weight[i])
+            s = ''
+            for j in range(40):
+                #print(word[t[j]], ':', weight[i][t[j]])
+                s = s + ' ' + word[t[j]]
+            key.append(s)
+            
+        # print recommended courses based on skill sets matched  
+        recommended = []
+        for i in range(len(key)):
+            for word in skillsinput.split(' '):
+                if (word in key[i]):
+                    recommended.append(course_merged['course_id'][i] + ':' + course_merged['names'][i] + ',course rate:' + '%.2f' % (f_merged['Overall course rate'][i]))
+        print("COURSES RECOMMENDED:")
+        for i in recommended:
+            print(i)
+     
+    
+    
     def recommend_course(self, jobs_df, courses_df):
         # TODO(liwei)
         pass
